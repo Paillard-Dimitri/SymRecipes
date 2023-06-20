824c1588f5ca9cd20ec4717d28d4a79f75dbd51e
Dimitri PAILLARD <dimitri.paillard@okwind.fr>
2023-06-20 16:33:39 +0200

    Affichage de tout les ingrédients

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class IngredientController extends AbstractController
+{
+    /**
+     * Cette fonction affiche tout les ingrédients 
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+ 
+        $ingredient = $paginator->paginate(
+            $repository->findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredient,
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,19 +3,21 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ingredient;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $ingredient = new Ingredient();
-        $ingredient->setName('Ingredient #1')
-            ->setPrice(3.0);
-        $ingredient2 = new Ingredient();
-        $ingredient2->setName('Ingredient2')
-            ->setPrice(4.5);
+        $faker = Factory::create('fr_FR');
+        for ($i=1; $i <= 50; $i++) { 
+            $ingredient = new Ingredient();
+            $ingredient->setName($faker->word())
+                ->setPrice(rand(1, 200));
+            $manager->persist($ingredient);
+        }
 
         $manager->flush();
     }
