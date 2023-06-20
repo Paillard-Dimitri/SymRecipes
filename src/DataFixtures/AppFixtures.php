36870f16b27d8b06aba30ca3e3455af9e1769933
Dimitri PAILLARD <dimitri.paillard@okwind.fr>
2023-06-20 09:37:10 +0200

    Premier controller et ajout des fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $ingredient = new Ingredient();
+        $ingredient->setName('Ingredient #1')
+            ->setPrice(3.0);
+        $ingredient2 = new Ingredient();
+        $ingredient2->setName('Ingredient2')
+            ->setPrice(4.5);
+
+        $manager->flush();
+    }
+}
